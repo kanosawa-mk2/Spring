8a9418987b0c795436e130d096c5c854d61ae88d
kanosawa-mk2 <kazun999orz@gmail.com>
2020-07-30 17:22:44 +0900

    test

--- SpringSample/src/main/resources/data.sql
+++ SpringSample/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 /* 従業員テーブルのデータ（第３章で作成） */
 INSERT INTO employee (employee_id, employee_name, age) VALUES( 1, '山田 太郎', 30);
+INSERT INTO employee (employee_id, employee_name, age) VALUES( 2, '田中 花子', 22);
