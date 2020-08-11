9a92cebfbabb7d407fb8b3c2408e16cd10f8f664
kanos <kanos@DESKTOP-615JQEI>
2020-08-11 12:24:10 +0900

    データバインド実装

---
+++ SpringSample/src/main/java/com/example/demo/login/controller/SignupController.java
@@ -0,0 +1,58 @@
+package com.example.demo.login.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.login.domain.model.SignupForm;
+
+@Controller
+public class SignupController {
+
+	private Map<String, String> radioMarriage;
+
+	private Map<String, String> initRadioMarrige() {
+
+		Map<String, String> radio = new LinkedHashMap<String, String>();
+		radio.put("既婚", "true");
+		radio.put("未婚", "false");
+		return radio;
+	}
+
+	@GetMapping("/signup")
+	public String getSignUp(@ModelAttribute SignupForm form, Model model) {
+
+		//@ModelAttributeをつけると自動でMpdelクラスに登録してくれる
+		// 以下のコードと同じ
+		//model.addAttribute("SignupForm", form);
+
+		// @ModelAttributeでは、デフォルトでクラス名の最初の文字を小文字に変えた文字列がキー名に登録される。
+
+		radioMarriage = initRadioMarrige();
+
+		model.addAttribute("radioMarriage", radioMarriage);
+
+		return "login/signup";
+	}
+
+	@PostMapping("/signup")
+	public String postSignUp(@ModelAttribute SignupForm form,BindingResult bindingResult, Model model) {
+		//データバインド結果の受け取りにはBindingResultクラスを追加する
+
+		if(bindingResult.hasErrors()) {
+			// GETリクエストのメソッドでユーザ登録画面に戻る
+			return getSignUp(form, model);
+		}
+		System.out.println(form);
+
+		// login.htmlにリダイレクト
+		return "redirect:/login";
+	}
+
+}
